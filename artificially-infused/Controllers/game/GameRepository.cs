0babbb67d457e76db96528d21fe9f3100553545a
CEI.DOMAIN\ihurst <ihurst@ceiamerica.com>
2024-01-27 17:40:56 -0500

    serialize object to storage

--- artificially-infused/Controllers/game/GameRepository.cs
+++ artificially-infused/Controllers/game/GameRepository.cs
@@ -1,7 +1,10 @@
 ﻿using Azure;
 using Azure.Data.Tables;
+using System.Collections.Concurrent;
 using System.Threading.Tasks;
-
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
 namespace artificially_infused.Controllers.game 
 {
    
@@ -18,7 +21,7 @@ namespace artificially_infused.Controllers.game
 
         public async Task SaveGameAsync(Game game)
         {
-            await _tableClient.UpsertEntityAsync(game);
+            await _tableClient.UpsertEntityAsync(new GameEntity(game));
         }
 
         public async Task<Game> GetGameAsync(string gameId)
@@ -26,9 +29,16 @@ namespace artificially_infused.Controllers.game
             var partitionKey = gameId;
             var rowKey = gameId;
 
-            var gameEntity = await _tableClient.GetEntityAsync<Game>(partitionKey, rowKey);
-
-            return gameEntity;
+            GameEntity gameEntity = await _tableClient.GetEntityAsync<GameEntity>(partitionKey, rowKey);
+            var game = new Game();
+            game.Code = gameEntity.Code;
+            game.PartitionKey = gameEntity.PartitionKey;
+            game.RowKey = gameEntity.RowKey;
+            game.ETag = gameEntity.ETag;
+            game.Timestamp = gameEntity.Timestamp;
+            game.Players = JsonConvert.DeserializeObject<List<Player>>(gameEntity.Players);
+            game.Round = JsonConvert.DeserializeObject<Round>(gameEntity.Round);
+            return game;
         }
         public async Task DeleteGameAsync(string gameId)
         {
@@ -50,8 +60,30 @@ namespace artificially_infused.Controllers.game
             }
         }
     }
+    public class GameEntity : ITableEntity
+    {
+        public string Players { get; set; }
+        public string Round { get; set; }
+        public string Code { get; set; }
+        public GameEntity()
+        {
+        }
 
-   
-    
+        public GameEntity(Game game)
+        {
+            PartitionKey = game.PartitionKey;
+            RowKey = game.RowKey;
+            Players = JsonConvert.SerializeObject(game.Players);
+            Round = JsonConvert.SerializeObject(game.Round);
+            Code = game.Code;
+        }
+
+        public string PartitionKey { get; set; }
+        public string RowKey { get; set; }
+        public DateTimeOffset? Timestamp { get; set; }
+        public ETag ETag { get; set; }
+    }
+
+
 
 }
