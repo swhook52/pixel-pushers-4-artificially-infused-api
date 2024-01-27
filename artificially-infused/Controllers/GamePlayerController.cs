a764afa101d80f7eb6397b2d15bfc5c01b952150
Steven Hook <steven@hookscode.com>
2024-01-27 15:56:27 -0500

    Add GamePlayerController for adding or removing a player from the game

---
+++ artificially-infused/Controllers/GamePlayerController.cs
@@ -0,0 +1,33 @@
+﻿using artificially_infused.Controllers.tv;
+using artificially_infused.Services;
+using Microsoft.AspNetCore.Mvc;
+
+namespace artificially_infused.Controllers
+{
+    [ApiController]
+    public class GamePlayerController
+    {
+        public GameService _gameService;
+
+        public GamePlayerController(GameService gameService)
+        {
+            _gameService = gameService;
+        }
+
+        [HttpPost("game/{gameId}/player")]
+        [ProducesResponseType(StatusCodes.Status204NoContent)]
+        public IActionResult Create(string gameId, [FromBody] Player player)
+        {
+            _gameService.AddPlayerToGame(gameId, player);
+            return new NoContentResult();
+        }
+
+        [HttpDelete("game/{gameId}/player/{playerId}")]
+        [ProducesResponseType(StatusCodes.Status204NoContent)]
+        public IActionResult Delete(string gameId, string playerId)
+        {
+            _gameService.DeletePlayerFromGame(gameId, playerId);
+            return new NoContentResult();
+        }
+    }
+}
