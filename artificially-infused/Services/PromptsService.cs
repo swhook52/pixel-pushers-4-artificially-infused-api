dac0b6607e953ac654574c7118d0a4635f19f17a
tbumbarger <tbumbarger@gmail.com>
2024-01-27 17:37:50 -0500

    Add data

---
+++ artificially-infused/Services/PromptsService.cs
@@ -0,0 +1,22 @@
+﻿using artificially_infused.Controllers.game;
+
+
+namespace artificially_infused.Services
+{
+    public class PromptsService
+    {
+        private readonly PromptsRepository _promptsRepository;
+        public PromptsService(PromptsRepository gameRepo)
+        {
+            _promptsRepository = gameRepo;
+        }
+
+        public string getRandomPrompt()
+        {
+            Random rnd = new Random();
+            int num = rnd.Next(_promptsRepository.Prompts.Count);
+            return _promptsRepository.Prompts[num];
+        }
+
+    }
+}
